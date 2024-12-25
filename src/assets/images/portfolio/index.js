1fc35bba03ea8080acf98a5ddde334f088783bb7
Jessica Nguyen <89553284+Jnguye84@users.noreply.github.com>
2024-12-24 19:27:52 -0500

    Update index.js

--- src/assets/images/portfolio/index.js
+++ src/assets/images/portfolio/index.js
@@ -3,7 +3,6 @@ import outlandish from './outlandish.png'
 import theater from './theater.png'
 import doc from './doc.jpg'
 import moviesml from './movie card trading game.mp4'
-import filmp from './filmpotpourri.jpg'
 import biotech from './biotech.mp4'
 import episode from './episode.jpg'
 import mitreality from './mitreality.mp4'
@@ -11,7 +10,6 @@ import fun from './fun.mp4'
 export {
     fun,
     harvard,
-    filmp,
     outlandish,
     doc,
     moviesml,
